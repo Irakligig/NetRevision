6a1d558e83aaa3c896f234818b9bacc1013e0380
Irakligig <darkbrineyt@gmail.com>
2025-04-19 17:45:02 +0400

    Add project files.

---
+++ Day1/Day1/Factorials.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class Factorials
+    {
+        public static void ultraFactorial(int x)
+        {
+            Console.WriteLine($"factorial is {factorial(x)}");
+            Console.WriteLine($"Double factorial is {doublefactorial(x)}");
+            Console.WriteLine($"Superfactorial is {superfactorial(x)}");
+        }
+
+        public static int factorial(int x)
+        {
+            if (x == 0 || x == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return x * factorial(x - 1);
+            }
+        }
+
+        public static int doublefactorial(int x)
+        {
+            int result = 1;
+            for (int i = x; i > 0; i -= 2)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        public static long superfactorial(int x)
+        {
+            long result = 1;
+            int pointer = 1;
+            while (pointer <= x)
+            {
+                result *= factorial(pointer);
+                pointer++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Day1/Day1/NumberGuessingGame.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class NumberGuessingGame
+    {
+        /*
+         1.inputad shemodis ragac ricxvi
+         2. outputi aqvs 5 nairi -- 
+        too high , high , close
+        low , too low
+        3. keep track of attempts 
+        4. keep track of win/loss stats
+         */
+
+        public static void Simulation()
+        {
+            Console.WriteLine("Hello Player");
+            int attempts = 0;
+            Random rnd = new Random();
+            int SecretNumber = rnd.Next(1, 201);
+            int previousGuess;
+
+            while (attempts < 7)
+            {
+                Console.WriteLine("Enter Number from 1 to 200");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess - SecretNumber > 20)
+                {
+                    Console.WriteLine("Too high");
+                    previousGuess = guess;
+                    attempts++;
+                }
+                else if (guess - SecretNumber > 10 && guess - SecretNumber <= 20)
+                {
+                    Console.WriteLine("High");
+                    previousGuess = guess;
+                    attempts++;
+                }
+                else if (guess - SecretNumber <= 10 && guess - SecretNumber > 0)
+                {
+                    Console.WriteLine("Close-High");
+                    previousGuess = guess;
+                    attempts++;
+                }
+                else if (SecretNumber - guess <= 10 && SecretNumber - guess > 0)
+                {
+                    Console.WriteLine("Close-low");
+                    attempts++;
+                }
+                else if(SecretNumber - guess > 10 && SecretNumber - guess <= 20)
+                {
+                     Console.WriteLine("Low");
+                    attempts++;
+                }
+                else if (SecretNumber - guess > 20)
+                {
+                    Console.WriteLine("Too low");
+                    attempts++;
+                }
+                else
+                {
+                    Console.WriteLine("you are right");
+                    Console.WriteLine("game is over");
+                    return;
+                }
+            }
+            
+            
+             
+            
+        }
+
+        
+    }
+}
